2e9cb6f3c45f2cf40ae34bb0f8f1c7ae4bbc3843
PolishChowChow <polishchowchow@gmail.com>
2025-04-12 01:48:04 +0200

    refactor: removed useless app-routing component

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,19 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { IndexPageComponent } from './app/index-page/index-page.component';
-import { LoginPageComponent } from './app/login-page/login-page.component';
-import { NotFoundPageComponent } from './app/not-found-page/not-found-page.component';
-import { RecoveryPasswordPageComponent } from './app/recovery-password-page/recovery-password-page.component';
-import { RegisterPageComponent } from './app/register-page/register-page.component';
 
-
-const routes: Routes = [
-  { path: 'login', component: LoginPageComponent },
-  { path: 'register', component: RegisterPageComponent },
-  { path: 'recovery-password', component: RecoveryPasswordPageComponent },
-  { path: '', component: IndexPageComponent },
-  { path: '**', component: NotFoundPageComponent },
+export const routes: Routes = [
+  { path: 'login', loadComponent: () => import('./app/login-page/login-page.component').then(m => m.LoginPageComponent)  },
+  { path: 'register', loadComponent: () => import('./app/register-page/register-page.component').then(m => m.RegisterPageComponent)  },
+  { path: 'recovery-password', loadComponent: () => import('./app/recovery-password-page/recovery-password-page.component').then(m => m.RecoveryPasswordPageComponent)  },
+  { path: '', loadComponent: () => import('./app/index-page/index-page.component').then(m => m.IndexPageComponent)  },
+  { path: '**', loadComponent: () => import('./app/not-found-page/not-found-page.component').then(m => m.NotFoundPageComponent) },
 ];
 
 bootstrapApplication(AppComponent, {
