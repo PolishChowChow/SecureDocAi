17f0035b20a3d8dde2dc590269b18f6b81c39166
PolishChowChow <polishchowchow@gmail.com>
2025-04-12 04:14:59 +0200

    feat: implemented changing input color partialy

--- client/src/app/index-page/edit-doc/edit-doc.component.ts
+++ client/src/app/index-page/edit-doc/edit-doc.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-edit-doc',
   standalone: false,
   templateUrl: './edit-doc.component.html',
-  styleUrl: './edit-doc.component.scss'
+  styleUrl: './edit-doc.component.scss',
 })
 export class EditDocComponent {
+  content = 'sss';
+  sanitizer = inject(DomSanitizer);
+  innerHTML: SafeHtml = '';
+  isConfigChanged = true;
+  contentList: SafeHtml[] = []
+  config1 = (value: string) => {
+    return `<span style="color: red">${value}</span>`;
+  };
+  config2 = (value: string) => {
+    return `<span style="color: black">${value}</span>`;
+  };
+  currentConfig = this.config1
+  updateConfig(){
+    this.currentConfig = this.isConfigChanged ? this.config1 : this.config2
+  }
 
+  onTextChange(value: string): void {
+    console.log(value);
+    
+    this.innerHTML = this.sanitizer.bypassSecurityTrustHtml(
+       this.currentConfig(value)
+    );
+  }
+  handleClick(){
+    console.log("clicked");
+    console.log(this.contentList);
+  
+    this.contentList.push(this.sanitizer.bypassSecurityTrustHtml(this.currentConfig(this.content)))
+    this.innerHTML = ""
+    this.content = ""
+    this.isConfigChanged = !this.isConfigChanged;
+    this.updateConfig();
+  }
+  
 }
--- client/src/app/index-page/edit-doc/edit-doc.module.ts
+++ client/src/app/index-page/edit-doc/edit-doc.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditDocComponent } from './edit-doc.component';
 import { EditDocRoutingModule } from './edit.doc.routing.module';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { EditDocRoutingModule } from './edit.doc.routing.module';
   declarations: [EditDocComponent],
   imports: [
     CommonModule,
-    EditDocRoutingModule
+    EditDocRoutingModule,
+    FormsModule
   ]
 })
 export class EditDocModule { }
