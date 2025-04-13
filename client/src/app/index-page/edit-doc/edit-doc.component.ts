6bbfb61fb1d5857db108f967945e88094b774126
PolishChowChow <polishchowchow@gmail.com>
2025-04-13 09:22:09 +0200

    feat: implement doc edit with multiple random parameters

--- client/src/app/index-page/edit-doc/edit-doc.component.ts
+++ client/src/app/index-page/edit-doc/edit-doc.component.ts
@@ -1,4 +1,12 @@
-import { Component, inject, input } from '@angular/core';
+import { sanitizeIdentifier } from '@angular/compiler';
+import {
+  Component,
+  ElementRef,
+  inject,
+  input,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
@@ -7,39 +15,42 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
   templateUrl: './edit-doc.component.html',
   styleUrl: './edit-doc.component.scss',
 })
-export class EditDocComponent {
-  content = 'sss';
-  sanitizer = inject(DomSanitizer);
-  innerHTML: SafeHtml = '';
-  isConfigChanged = true;
-  contentList: SafeHtml[] = []
-  config1 = (value: string) => {
-    return `<span style="color: red">${value}</span>`;
-  };
-  config2 = (value: string) => {
-    return `<span style="color: black">${value}</span>`;
-  };
-  currentConfig = this.config1
-  updateConfig(){
-    this.currentConfig = this.isConfigChanged ? this.config1 : this.config2
+export class EditDocComponent implements AfterViewInit {
+  @ViewChild('editor') editorRef!: ElementRef<HTMLDivElement>;
+  isFloatLeft = false;
+  setCaretToEnd(element: HTMLElement): void {
+    const range = document.createRange();
+    const selection = window.getSelection();
+    range.selectNodeContents(element);
+    range.collapse(false);
+    selection?.removeAllRanges();
+    selection?.addRange(range);
+  }
+  ngAfterViewInit(): void {
+    this.editorRef.nativeElement.innerHTML = `<div style="color: red" contenteditable="true">`
   }
 
-  onTextChange(value: string): void {
-    console.log(value);
-    
-    this.innerHTML = this.sanitizer.bypassSecurityTrustHtml(
-       this.currentConfig(value)
-    );
+  handleEditorClick() {
+    this.editorRef.nativeElement.contentEditable = 'true';
   }
-  handleClick(){
-    console.log("clicked");
-    console.log(this.contentList);
-  
-    this.contentList.push(this.sanitizer.bypassSecurityTrustHtml(this.currentConfig(this.content)))
-    this.innerHTML = ""
-    this.content = ""
-    this.isConfigChanged = !this.isConfigChanged;
-    this.updateConfig();
+
+  toggleParser() {
+    console.log('btn');
+    this.isFloatLeft = !this.isFloatLeft;
+    this.editorRef.nativeElement.innerHTML += `</div><div style="display: inline; color: ${this.randomizeColor()}; font-size: ${this.randomizeSize()}px; text-align: ${
+      this.isFloatLeft ? 'left' : 'right'
+    }" contenteditable="true">s`;
+    this.editorRef.nativeElement.innerHTML
+    this.setCaretToEnd(this.editorRef.nativeElement);
   }
-  
+  randomizeSize() {
+    return (Math.random() * 30 + 10).toFixed(0);
+  }
+  randomizeColor() {
+    const num1 = (Math.random() * 255).toFixed(0);
+    const num2 = (Math.random() * 255).toFixed(0);
+    const num3 = (Math.random() * 255).toFixed(0);
+    return `rgb(${num1}, ${num2}, ${num3})`;
+  }
+  randomizeFloating() {}
 }
